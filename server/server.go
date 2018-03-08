4c8fc129d7917f93c709b0b0f1aec24ebc1a5c75
WindomZ <git.windomz@gmail.com>
2018-03-09 00:16:54 +0800

    feat: add restful api server

---
+++ server/server.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"net/http"
+
+	"github.com/WindomZ/quizzee"
+	"github.com/WindomZ/quizzee/quizzeer"
+)
+
+const port = 8080
+
+func handlerRoot(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	var req quizzee.Request
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&req); err != nil {
+		w.Write(req.Fail(10, err.Error()).Bytes())
+		return
+	}
+	defer r.Body.Close()
+
+	if req.Question == "" {
+		w.Write(req.Fail(20, "empty question").Bytes())
+		return
+	}
+	if len(req.Options) == 0 {
+		w.Write(req.Fail(21, "empty options").Bytes())
+		return
+	}
+
+	if req.Answer != "" {
+		err := quizzeer.Mark(req.Question, req.Options, req.Answer)
+		if err != nil {
+			w.Write(req.Fail(30, err.Error()).Bytes())
+			return
+		}
+		req.Accuracy = 1 // 100%
+	} else {
+		recommend, rates := quizzeer.Recommend(req.Question, req.Options)
+		req.Answer = req.Options[recommend]
+		req.Accuracy = rates[recommend]
+	}
+
+	w.Write(req.Success().Bytes())
+}
+
+func main() {
+	var tableName, dataPath string
+	flag.StringVar(&tableName, "t", "quizzee", "the table name of database")
+	flag.StringVar(&dataPath, "f", "./data/quizzee.db", "the path of database file")
+
+	flag.Parse()
+
+	fmt.Printf("load the database: '%s' in '%s'\n", tableName, dataPath)
+
+	quizzeer.RegisterDB(tableName, dataPath)
+	defer quizzeer.CloseDB()
+
+	fmt.Println("listen the port:", port)
+
+	http.HandleFunc("/", handlerRoot)
+	if err := http.ListenAndServe(fmt.Sprintf(":%v", port), nil); err != nil {
+		panic(err)
+	}
+}
